ffab8344a2cc0a78903bce1f428f6b232167091b
Erik Karsten <erik@opcode.xyz>
2026-04-06 08:32:27 +0000

    wip: add askama templating

--- src/commands/new.rs
+++ src/commands/new.rs
@@ -1,8 +1,23 @@
 use color_eyre::eyre::Result;
-use std::process::Command;
 
-use crate::cli::Templates;
+use crate::cli::{Templates, TemplateArgs};
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "aspect.nix")]
+struct AspectTemplate<'a> {
+    args: &'a TemplateArgs,
+}
 
 pub fn new(ctx: crate::context::Context, template: Templates) -> Result<()> {
+    match template {
+        Templates::Aspect { args } => {
+            let aspect = AspectTemplate { args: &args }; 
+            println!("{}", aspect.render()?);
+        }
+
+        _ => todo!()
+    }
+
     Ok(())
 }
